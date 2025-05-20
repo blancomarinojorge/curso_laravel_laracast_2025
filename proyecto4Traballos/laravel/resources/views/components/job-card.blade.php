eb4d6efc820b168fe8d81628304b92a1eb1c1e8d
Jorge Blanco Mariño <blancomarinojorge@gmail.com>
2025-05-20 21:01:11 +0200

    layout proyecto4 en proceso

---
+++ proyecto4Traballos/laravel/resources/views/components/job-card.blade.php
@@ -0,0 +1,16 @@
+<div class="p-6 bg-white/10 rounded-xl flex flex-col text-center">
+    <div class="self-start">GovExec</div>
+    <div class="my-10 font-bold">
+        <h3>Full stack Laravel Developer</h3>
+        <div>Full Time - from 60000</div>
+    </div>
+    <div class="flex flex-row justify-between my-auto">
+        <div class="flex flex-row space-x-3 items-end">
+            <a href="#" class="px-3 py-1 bg-white/10 rounded-xl text-xs font-bold hover:bg-white/25 transition-colors duration-300">Job1</a>
+            <a href="#" class="px-3 py-1 bg-white/10 rounded-xl text-xs font-bold hover:bg-white/25 transition-colors duration-300">Job1</a>
+            <a href="#" class="px-3 py-1 bg-white/10 rounded-xl text-xs font-bold hover:bg-white/25 transition-colors duration-300">Job1</a>
+        </div>
+        <div><img src="https://placehold.co/42" alt="img" class="rounded-xl"></div>
+    </div>
+
+</div>
---
+++ proyecto4Traballos/laravel/resources/views/components/layout.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+    @vite(['resources/css/app.css','resources/js/app.js'])
+</head>
+<body class="bg-black text-white">
+<div class="px-10">
+    <nav class="flex flex-row justify-between py-5 align-middle">
+        <div>
+            <a href="">
+                <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="Logo">
+            </a>
+        </div>
+        <div>
+            <ul class="flex space-x-5 font-bold">
+                <li><a href="" >Jobs</a></li>
+                <li><a href="" >Outro</a></li>
+                <li><a href="" >Eii</a></li>
+                <li><a href="" >Jobs</a></li>
+            </ul>
+        </div>
+        <div>login</div>
+    </nav>
+    <main class="mt-10 max-w-[936px] mx-auto">
+        {{ $slot }}
+    </main>
+</div>
+</body>
+</html>
